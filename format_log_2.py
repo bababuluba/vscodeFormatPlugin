53b68508438290e23e9182c29130a4a0f0c5101c
James Wu <james.wu@invenco.com>
2023-05-31 16:26:27 +1200

    ok

--- format_log_2.py
+++ format_log_2.py
@@ -66,42 +66,38 @@ def reformat_xml_log(log_entries):
     return out
 def reformat_xml_log_1(log_entries):
     # Extract XML segments using regular expressions
-    pattern = r"(?:<\?xml[^>]+?><([^>\s]+)[\s>].*?</\1>)"
+    pattern = r"<\?xml[^>]+?><([^>\s]+)[\s>].*?</\1>"
     # Find all XML matches in the log entries
-    matchObj = re.findall(pattern, log_entries, re.X|re.M)
-    for tag in matchObj:
-        print (tag)
-        
-    # if matchObj:
-    #     print ("1")
-    #     print (matchObj.group())
-    #     print ("2")
-    #     print  (matchObj.group(1))
-    # else:
-    #     print ("No match!!")
-    #     xml = ET.fromstring(nx)
-    #     s = ET.tostring(xml,encoding='unicode',xml_declaration = True)
-    #     # xml_bytes = s.encode("utf-8")
-    #     reformatted_xml_bytes = html.unescape(s)
-    #     # reformatted_xml_string = reformatted_xml_bytes.decode("utf-8")  
-    #     # print (reformatted_xml_bytes)
-    #     dom = minidom.parseString(reformatted_xml_bytes)
+    matchObj = [match.group() for match in re.finditer(pattern, log_entries)]
+    for x in matchObj:    
+        match = re.search(r"&lt;\?xml.*?\?&gt;", x)
+        if match:
+            nx = re.sub(r"&lt;\?xml.*?\?&gt;", "verySpeical", x)
+        else:
+            nx = x
+        print(nx)
+        xml = ET.fromstring(nx)
+        s = ET.tostring(xml,encoding='unicode',xml_declaration = True)
+        # xml_bytes = s.encode("utf-8")
+        reformatted_xml_bytes = html.unescape(s)
+        # reformatted_xml_string = reformatted_xml_bytes.decode("utf-8")  
+        # print (reformatted_xml_bytes)
+        dom = minidom.parseString(reformatted_xml_bytes)
 
-    #     # Pretty-print the XML
-    #     pretty_xml = dom.toprettyxml(indent="  ")
-    #     if match:
-    #         substring = match.group()
-    #         substring = substring.replace("&lt;", "<")
-    #         substring = substring.replace("&gt;", ">")
-    #         pretty_xml = pretty_xml.replace("verySpeical", substring)
-    #     else:
-    #         pretty_xml = pretty_xml.replace("verySpeical", '<?xml version="1.0" encoding="UTF-8"?>')
+        # Pretty-print the XML
+        pretty_xml = dom.toprettyxml(indent="  ")
+        if match:
+            substring = match.group()
+            substring = substring.replace("&lt;", "<")
+            substring = substring.replace("&gt;", ">")
+            pretty_xml = pretty_xml.replace("verySpeical", substring)
+        else:
+            pretty_xml = pretty_xml.replace("verySpeical", '<?xml version="1.0" encoding="UTF-8"?>')
 
-    #     print(pretty_xml)
-    #     print("--------------")
-    #     out = log_entries.replace(x, "\n"+ pretty_xml)
-    # print(out)
-    # return out
+        print(pretty_xml)
+        print("--------------")
+        log_entries = log_entries.replace(x, "\n"+ pretty_xml)
+    return log_entries
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -115,5 +111,5 @@ if __name__ == '__main__':
         # print(formatted_text)
 
         with open(log_file_path, 'w') as log_file:
-            log_file.write(out)
+            log_file.write(formatted_text)
 
